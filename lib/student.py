0f61005e8d19944e0a851deef0a3b99e502fad0d
ussenuk <ukimanuka@gmail.com>
2024-01-28 11:04:10 +0300

    understanding the object inheritance concept

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,13 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, add_knowledge):
+        self.knowledge.append(add_knowledge)
+        return self.knowledge
+
+Ussen = Student("Ussen", "Kimanuka")
+print(Ussen.learn("React"))
--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,28 @@ from user import User
 import random
 
 class Teacher(User):
+    
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+        
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        r1 = random.randint(0,7)
+
+        random_lesson = self.knowledge[r1] 
+        return random_lesson
+
+
+Ciira = Teacher("Ciira", "Maina")
+print(Ciira.teach())
